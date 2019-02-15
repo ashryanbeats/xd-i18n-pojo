f906ffcb0a9c6daa1a4ad5fd10a85e9120aef2f6
Ash <ash@ashryanbeats.com>
2019-02-15 18:56:03 -0500

    Handling exits for OK, Cancel, and Escape.

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-function mainCommand() {
+async function mainCommand() {
+  const dialog = createDialog();
+
+  const result = await dialog.showModal();
+  if (result === "reasonCanceled") return;
+
+  console.log("Exited with OK!");
+
+  return;
+}
+
+function createDialog() {
   document.body.innerHTML = `
-    <dialog>
+    <style>
+      form {
+        width: 400px;
+      }
+    </style>
+    <dialog id="dialog">
       <form method="dialog">
+        <h1>Get me out of here</h1>
+        <p>Handling 3 standard outcomes for a modal: OK, Cancel, and Escape.
         <footer>
           <button uxp-variant="primary" id="cancel-button">Cancel</button>
           <button type="submit" uxp-variant="cta" id="ok-button">OK</button>
@@ -10,10 +28,11 @@ function mainCommand() {
     </dialog>
   `;
 
-  const dialog = document.querySelector("dialog");
-  dialog.showModal();
+  const cancelButton = document.querySelector("#cancel-button");
+  cancelButton.addEventListener("click", e => dialog.close("reasonCanceled"));
 
-  return;
+  const dialog = document.querySelector("#dialog");
+  return dialog;
 }
 
 module.exports = {
